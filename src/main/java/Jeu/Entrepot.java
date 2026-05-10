e49ed287b6ae7f99f10d2c637c7026d1acba33dc
aissa <aissaouameur@gmail.com>
2026-05-10 11:46:46 +0200

    Classe Entrepot : Codage de la classe Entrepot pour faire fonctionner la classe Monde

--- src/main/java/Jeu/Entrepot.java
+++ src/main/java/Jeu/Entrepot.java
@@ -1,48 +1,50 @@
 package Jeu;
 
-/**
- * 
- */
 public class Entrepot {
 
-    /**
-     * Default constructor
-     */
-    public Entrepot() {
-    }
-
-    /**
-     * 
-     */
+    // Attributs
     private int id;
-
-    /**
-     * 
-     */
-    private String Minerais;
-
-    /**
-     * 
-     */
+    private String minerais;
     private int capacite;
+    private int stock;
 
-
-    /**
-     * 
-     */
-    public Secteur Entrepot = Entrepot;
-
-
-    /**
-     * 
-     */
-    public Minerais Entrepot;
-
-    /**
-     * @param minerais
-     */
-    private void Stocker(Minerais minerais) {
-        // TODO implement here
+    // Constructeur
+    public Entrepot(int id, String minerais, int capacite) {
+        this.id = id;
+        this.minerais = minerais;
+        this.capacite = capacite;
+        this.stock = 0;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getMinerais() {
+        return minerais;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void ajouterStock(int quantite) {
+        this.stock += quantite;
+
+        // Si le stock dépasse la capacité maximale, on le ramène à la capacité
+        if (this.stock > this.capacite) {
+            this.stock = this.capacite;
+        }
+    }
+
+    // Affichage
+    @Override
+    public String toString() {
+        return "Entrepot[id=" + id + ", type=" + minerais +
+                ", stock=" + stock + "/" + capacite + "]";
+    }
 }
