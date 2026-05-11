b3f6f7cd45be7192b02862752568c90377320dbd
Ouameur <aissaouameur@gmail.com>
2026-05-11 09:38:35 +0200

    Fin de code de la classe monde et de toute les méthodes

--- src/main/java/Jeu/Monde.java
+++ src/main/java/Jeu/Monde.java
@@ -2,6 +2,7 @@ package Jeu;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Monde {
     private Secteur[][] grille;
@@ -149,14 +150,99 @@ public class Monde {
 
 
     public void tour() {
-        // TODO implement here
+        Scanner clavier = new Scanner(System.in);
+
+        for (Robot robot : lesRobots) {
+            System.out.println("Action pour le robot " + robot.getId() + " (" + robot.getType() + ") ?");
+            System.out.println("(Tapez : nord, sud, est, ouest, recolter, deposer)");
+
+            // On lit et on met tout en minuscules pour éviter les erreurs de frappe (Nord, NORD, nord...)
+            String action = clavier.nextLine().toLowerCase();
+
+            // On mémorise la position actuelle du robot
+            int ancienneLigne = robot.getLigne();
+            int ancienneColonne = robot.getColonne();
+            int nouvelleLigne = ancienneLigne;
+            int nouvelleColonne = ancienneColonne;
+
+            // On calcule les coordonnées d'arrivée en fonction de la direction
+            if (action.equals("nord")) {
+                nouvelleLigne = ancienneLigne - 1;
+            } else if (action.equals("sud")) {
+                nouvelleLigne = ancienneLigne + 1;
+            } else if (action.equals("est")) {
+                nouvelleColonne = ancienneColonne + 1;
+            } else if (action.equals("ouest")) {
+                nouvelleColonne = ancienneColonne - 1;
+            } else if (action.equals("recolter")) {
+                // Le robot récolte sur sa case actuelle
+                robot.recolter();
+                continue; // Action terminée, on passe directement au robot suivant
+            } else if (action.equals("deposer")) {
+                // Le robot dépose sur sa case actuelle
+                robot.deposer();
+                continue; // Action terminée, on passe au robot suivant
+            }
+
+            // 1. On vérifie qu'on ne sort pas de la carte (entre 0 et 9)
+            if (nouvelleLigne >= 0 && nouvelleLigne <= 9 && nouvelleColonne >= 0 && nouvelleColonne <= 9) {
+
+                // 2. On vérifie la case d'arrivée : pas d'eau ET pas de robot
+                if (grille[nouvelleLigne][nouvelleColonne].getEau() == null && grille[nouvelleLigne][nouvelleColonne].getRobot() == null) {
+
+                    // On l'enlève de son ancienne case sur la grille
+                    grille[ancienneLigne][ancienneColonne].setRobot(null);
+
+                    // Le robot modifie ses propres variables internes (ligne et colonne)
+                    robot.avancer(action);
+
+                    // C. On l'ajoute sur sa nouvelle case sur la grille
+                    grille[nouvelleLigne][nouvelleColonne].setRobot(robot);
+                    System.out.println("Déplacement réussi !");
+                } else {
+                    System.out.println("Action impossible : il y a de l'eau ou un autre robot !");
+                }
+            } else {
+                System.out.println("Action impossible : vous touchez le bord du monde !");
+            }
+        }
     }
 
     /**
      *
      */
     public void affichermonde() {
-        // TODO implement here
+        System.out.println("\n--- CARTE DU MONDE ---");
+
+        for (int i = 0; i < this.grille.length; i++) {
+            for (int j = 0; j < this.grille[i].length; j++) {
+
+                Secteur caseActuelle = grille[i][j];
+
+                // 1. Priorité au Robot
+                if (caseActuelle.getRobot() != null) {
+                    System.out.print("[R]");
+                }
+                // 2. Les Entrepôts
+                else if (caseActuelle.getEntrepot() != null) {
+                    System.out.print("[E]");
+                }
+                // 3. Les Mines
+                else if (caseActuelle.getMine() != null) {
+                    System.out.print("[M]");
+                }
+                // 4. L'Eau
+                else if (caseActuelle.getEau() != null) {
+                    System.out.print("[~]");
+                }
+                else {
+                    System.out.print("[ ]");
+                }
+            }
+
+            System.out.println();
+        }
+        System.out.println("----------------------\n");
     }
 
 }
