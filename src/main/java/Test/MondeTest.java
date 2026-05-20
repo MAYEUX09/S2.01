e3051da3e10be026d6b91ccc830e7d85deaad381
axel53p <vanachter.axel@gmail.com>
2026-05-20 10:55:47 +0200

    test

--- src/main/java/Test/MondeTest.java
+++ src/main/java/Test/MondeTest.java
@@ -1,355 +1,143 @@
 package Test;
-import Jeu.*;
 
+import Jeu.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MondeTest {
-        private Mine mineOr;
-        private Mine mineNickel;
-        private Entrepot entrepotOr;
-        private Entrepot entrepotNickel;
-        private Robot robotOr;
-        private Robot robotNickel;
-        private Monde monde;
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
-    @Test
-    void testCreationMineOR() {
-        assertEquals(1, mineOR.getId());
-        assertEquals("OR", mineOR.getTypeMinerai());
-        assertEquals(80, mineOR.getCapaciteActuel());
-        assertEquals(80, mineOR.getCapaciteMax());
-    }
+class MondeTest {
 
-    @Test
-    void testExtractionNormaleMine() {
-        int quantiteExtraite = mineOR.extraire(10);
-
-        assertEquals(10, quantiteExtraite);
-        assertEquals(70, mineOR.getCapaciteActuel());
-    }
-
-    @Test
-    void testExtractionSuperieureStockMine() {
-        int quantiteExtraite = mineNI.extraire(100);
-
-        assertEquals(60, quantiteExtraite);
-        assertEquals(0, mineNI.getCapaciteActuel());
-    }
-
-    @Test
-    void testDeplacementRobotVersEst() {
-        robotOR.avancer("est");
-
-        assertEquals(0, robotOR.getLigne());
-        assertEquals(1, robotOR.getColonne());
-    }
-
-    @Test
-    void testDeplacementRobotVersSud() {
-        robotOR.avancer("sud");
-
-        assertEquals(1, robotOR.getLigne());
-        assertEquals(0, robotOR.getColonne());
-    }
-
-    @Test
-    void testRecolteBonneMine() {
-        int recolte = robotOR.recolter(mineOR);
-
-        assertEquals(3, recolte);
-        assertEquals(3, robotOR.getStockActuel());
-        assertEquals(77, mineOR.getCapaciteActuel());
-    }
-
-    @Test
-    void testRecolteMauvaisTypeMine() {
-        int recolte = robotOR.recolter(mineNI);
-
-        assertEquals(0, recolte);
-        assertEquals(0, robotOR.getStockActuel());
-        assertEquals(60, mineNI.getCapaciteActuel());
-    }
-
-    @Test
-    void testRecolteJusquaCapaciteMaxRobot() {
-        int recolte1 = robotOR.recolter(mineOR);
-        int recolte2 = robotOR.recolter(mineOR);
-        int recolte3 = robotOR.recolter(mineOR);
-
-        assertEquals(3, recolte1);
-        assertEquals(3, recolte2);
-        assertEquals(1, recolte3);
-        assertEquals(7, robotOR.getStockActuel());
-        assertEquals(73, mineOR.getCapaciteActuel());
-    }
-
-    @Test
-    void testDeposerDansBonEntrepot() {
-        robotOR.recolter(mineOR);
-        int quantiteDeposee = robotOR.deposer(entrepotOR);
-
-        assertEquals(3, quantiteDeposee);
-        assertEquals(0, robotOR.getStockActuel());
-        assertEquals(3, entrepotOR.getStockActuel());
-    }
-
-    @Test
-    void testDeposerDansMauvaisEntrepot() {
-        robotOR.recolter(mineOR);
-        int quantiteDeposee = robotOR.deposer(entrepotNI);
-
-        assertEquals(0, quantiteDeposee);
-        assertEquals(3, robotOR.getStockActuel());
-        assertEquals(0, entrepotNI.getStockActuel());
-    }
-
-    @Test
-    void testDeposerSacVide() {
-        int quantiteDeposee = robotOR.deposer(entrepotOR);
-
-        assertEquals(0, quantiteDeposee);
-        assertEquals(0, robotOR.getStockActuel());
-        assertEquals(0, entrepotOR.getStockActuel());
-    }
-
-    @Test
-    void testStockageEntrepot() {
-        entrepotOR.stocker(5);
-        entrepotOR.stocker(2);
-
-        assertEquals(7, entrepotOR.getStockActuel());
-    }
-
-    @Test
-    void testSecteurTerrain() {
-        assertNotNull(secteurTerrain.getTerrain());
-        assertNull(secteurTerrain.getEau());
-        assertNull(secteurTerrain.getMine());
-        assertNull(secteurTerrain.getEntrepot());
-        assertNull(secteurTerrain.getRobot());
-    }
-
-    @Test
-    void testSecteurEau() {
-        assertNotNull(secteurEau.getEau());
-        assertNull(secteurEau.getTerrain());
-        assertNull(secteurEau.getMine());
-        assertNull(secteurEau.getEntrepot());
-        assertNull(secteurEau.getRobot());
-    }
-
-    @Test
-    void testAjouterMineEntrepotRobotDansSecteurTerrain() {
-        secteurTerrain.setMine(mineOR);
-        secteurTerrain.setEntrepot(entrepotOR);
-        secteurTerrain.setRobot(robotOR);
-
-        assertEquals(mineOR, secteurTerrain.getMine());
-        assertEquals(entrepotOR, secteurTerrain.getEntrepot());
-        assertEquals(robotOR, secteurTerrain.getRobot());
-    }
-
-// Partie 2
-    private Mine mineOR;
-    private Mine mineNI;
-    private Robot robotOR;
-    private Entrepot entrepotOR;
-    private Entrepot entrepotNI;
-    private Secteur secteurTerrain;
-    private Secteur secteurEau;
+    protected Monde monde;
+    protected Secteur[][] grille;
+    protected ArrayList<Robot> lesRobots;
 
     @BeforeEach
     void setUp() {
-        mineOR         = new Mine(1, "OR", 80, 80);
-        mineNI         = new Mine(2, "NI", 60, 60);
-        robotOR        = new Robot(1, "OR", 7, 3, 0, 0);
-        entrepotOR     = new Entrepot(1, "OR", 5, 5);
-        entrepotNI     = new Entrepot(2, "NI", 8, 2);
-        secteurTerrain = new Secteur(new Terrain(0));
-        secteurEau     = new Secteur(new Eau());
+        monde = new Monde();
+
+        grille = new Secteur[10][10];
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                grille[i][j] = new Secteur(new Terrain(i * 10 + j));
+
+        lesRobots = new ArrayList<>();
+
+        monde.setGrille(grille);
+        monde.setLesRobots(lesRobots);
+        monde.setNumeroTour(1);
     }
 
     @AfterEach
-    void tearDown() {
-        mineOR = null; mineNI = null;
-        robotOR = null;
-        entrepotOR = null; entrepotNI = null;
-        secteurTerrain = null; secteurEau = null;
+    void tearDown() throws Exception {
+        monde = null;
+        grille = null;
+        lesRobots = null;
     }
 
-        //Mine
-
+    // Cas 1 - C1=V : action "recolter" -> recolter() appelé
     @Test
-    void testExtraireNormal() {
-            assertEquals(5, mineOR.extraire(5));
-            assertEquals(75, mineOR.getCapaciteActuel());
-        }
+    void testTour1() {
+        Mine mine = new Mine(1, "OR", 80, 80);
+        Robot robot = new Robot(1, "OR", 7, 3, 5, 5);
+        grille[5][5].setMine(mine);
+        grille[5][5].setRobot(robot);
+        lesRobots.add(robot);
 
-        @Test
-        void testExtraireExact() {
-            assertEquals(80, mineOR.extraire(80));
-            assertEquals(0, mineOR.getCapaciteActuel());
-        }
+        System.setIn(new ByteArrayInputStream("recolter\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testExtrairePartiel() {
-            Mine minePetite = new Mine(3, "OR", 50, 3);
-            assertEquals(3, minePetite.extraire(5));
-            assertEquals(0, minePetite.getCapaciteActuel());
-        }
+        assertEquals(3, robot.getStockActuel());
+        assertEquals(77, mine.getCapaciteActuel());
+    }
 
-        @Test
-        void testExtraireMineVide() {
-            Mine mineVide = new Mine(4, "OR", 50, 0);
-            assertEquals(0, mineVide.extraire(5));
-        }
+    // Cas 2 - C1=F, C2=V : action "deposer" -> deposer() appelé
+    @Test
+    void testTour2() {
+        Mine mine = new Mine(1, "OR", 80, 80);
+        Entrepot entrepot = new Entrepot(1, "OR", 5, 5);
+        Robot robot = new Robot(1, "OR", 7, 3, 5, 5);
+        grille[5][5].setMine(mine);
+        grille[5][5].setEntrepot(entrepot);
+        grille[5][5].setRobot(robot);
+        lesRobots.add(robot);
 
-        @Test
-        void testExtraireZero() {
-            assertEquals(0, mineOR.extraire(0));
-            assertEquals(80, mineOR.getCapaciteActuel());
-        }
+        // On récolte d'abord pour avoir du stock à déposer
+        robot.recolter(mine);
 
-        //Robot
+        System.setIn(new ByteArrayInputStream("deposer\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testRecolterNormal() {
-            assertEquals(3, robotOR.recolter(mineOR));
-            assertEquals(3, robotOR.getStockActuel());
-        }
+        assertEquals(0, robot.getStockActuel());
+        assertEquals(3, entrepot.getStockActuel());
+    }
 
-        @Test
-        void testRecolterMauvaisType() {
-            assertEquals(0, robotOR.recolter(mineNI));
-            assertEquals(0, robotOR.getStockActuel());
-        }
+    // Cas 3 - C1=F, C2=F, C3=V, C4=V, C5=V : déplacement possible -> robot déplacé
+    @Test
+    void testTour3() {
+        Robot robot = new Robot(1, "OR", 7, 3, 5, 5);
+        grille[5][5].setRobot(robot);
+        lesRobots.add(robot);
 
-        @Test
-        void testRecolterSacPlein() {
-            robotOR.recolter(mineOR); // stock = 3
-            robotOR.recolter(mineOR); // stock = 6
-            robotOR.recolter(mineOR); // stock = 7 (sac plein)
-            assertEquals(0, robotOR.recolter(mineOR));
-        }
+        System.setIn(new ByteArrayInputStream("est\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testRecolterMinePresqueVide() {
-            Mine minePetite = new Mine(3, "OR", 50, 1);
-            assertEquals(1, robotOR.recolter(minePetite));
-        }
+        assertNull(grille[5][5].getRobot());
+        assertEquals(robot, grille[5][6].getRobot());
+        assertEquals(5, robot.getLigne());
+        assertEquals(6, robot.getColonne());
+    }
 
-        @Test
-        void testRecolterMineNull() {
-            assertEquals(0, robotOR.recolter(null));
-        }
+    // Cas 4 - C1=F, C2=F, C3=F : hors limites -> mouvement impossible
+    @Test
+    void testTour4() {
+        Robot robot = new Robot(1, "OR", 7, 3, 0, 0);
+        grille[0][0].setRobot(robot);
+        lesRobots.add(robot);
 
-        @Test
-        void testDeposerNormal() {
-            robotOR.recolter(mineOR); // stock = 3
-            assertEquals(3, robotOR.deposer(entrepotOR));
-            assertEquals(0, robotOR.getStockActuel());
-            assertEquals(3, entrepotOR.getStockActuel());
-        }
+        System.setIn(new ByteArrayInputStream("ouest\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testDeposerMauvaisType() {
-            robotOR.recolter(mineOR); // stock = 3
-            assertEquals(0, robotOR.deposer(entrepotNI));
-            assertEquals(3, robotOR.getStockActuel());
-        }
+        assertEquals(robot, grille[0][0].getRobot());
+        assertEquals(0, robot.getLigne());
+        assertEquals(0, robot.getColonne());
+    }
 
-        @Test
-        void testDeposerRobotVide() {
-            assertEquals(0, robotOR.deposer(entrepotOR));
-        }
+    // Cas 5 - C1=F, C2=F, C3=V, C4=F : case d'eau -> mouvement impossible
+    @Test
+    void testTour5() {
+        Robot robot = new Robot(1, "OR", 7, 3, 4, 4);
+        grille[4][4].setRobot(robot);
+        grille[4][5] = new Secteur(new Eau());
+        lesRobots.add(robot);
 
-        @Test
-        void testDeposerEntrepotNull() {
-            robotOR.recolter(mineOR);
-            assertEquals(0, robotOR.deposer(null));
-            assertEquals(3, robotOR.getStockActuel());
-        }
+        System.setIn(new ByteArrayInputStream("est\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testAvancerNord() {
-            robotOR.avancer("nord");
-            assertEquals(-1, robotOR.getLigne());
-        }
+        assertEquals(robot, grille[4][4].getRobot());
+        assertEquals(4, robot.getLigne());
+        assertEquals(4, robot.getColonne());
+    }
 
-        @Test
-        void testAvancerSud() {
-            robotOR.avancer("sud");
-            assertEquals(1, robotOR.getLigne());
-        }
+    // Cas 6 - C1=F, C2=F, C3=V, C4=V, C5=F : case occupée par un robot -> mouvement impossible
+    @Test
+    void testTour6() {
+        Robot robot1 = new Robot(1, "OR", 7, 3, 3, 3);
+        Robot robot2 = new Robot(2, "NI", 7, 3, 3, 4);
+        grille[3][3].setRobot(robot1);
+        grille[3][4].setRobot(robot2);
+        lesRobots.add(robot1);
 
-        @Test
-        void testAvancerEst() {
-            robotOR.avancer("est");
-            assertEquals(1, robotOR.getColonne());
-        }
+        System.setIn(new ByteArrayInputStream("est\n".getBytes()));
+        monde.tour();
 
-        @Test
-        void testAvancerOuest() {
-            robotOR.avancer("ouest");
-            assertEquals(-1, robotOR.getColonne());
-        }
-
-        //Entrepot
-
-        @Test
-        void testStockerPremierDepot() {
-            entrepotOR.stocker(10);
-            assertEquals(10, entrepotOR.getStockActuel());
-        }
-
-        @Test
-        void testStockerCumulatif() {
-            entrepotOR.stocker(10);
-            entrepotOR.stocker(5);
-            assertEquals(15, entrepotOR.getStockActuel());
-        }
-
-        @Test
-        void testStockerZero() {
-            entrepotOR.stocker(0);
-            assertEquals(0, entrepotOR.getStockActuel());
-        }
-
-        //Secteur
-
-        @Test
-        void testSecteurTerrainVide() {
-            assertNull(secteurTerrain.getMine());
-            assertNull(secteurTerrain.getEntrepot());
-            assertNull(secteurTerrain.getRobot());
-            assertNull(secteurTerrain.getEau());
-        }
-
-        @Test
-        void testSecteurEauPresente() {
-            assertNotNull(secteurEau.getEau());
-        }
-
-        @Test
-        void testSecteurSetMine() {
-            secteurTerrain.setMine(mineOR);
-            assertEquals(mineOR, secteurTerrain.getMine());
-        }
-
-        @Test
-        void testSecteurSetRobot() {
-            secteurTerrain.setRobot(robotOR);
-            assertEquals(robotOR, secteurTerrain.getRobot());
-        }
-
-        @Test
-        void testSecteurMineEtRobotSimultanes() {
-            secteurTerrain.setMine(mineOR);
-            secteurTerrain.setRobot(robotOR);
-            assertNotNull(secteurTerrain.getMine());
-            assertNotNull(secteurTerrain.getRobot());
+        assertEquals(robot1, grille[3][3].getRobot());
+        assertEquals(robot2, grille[3][4].getRobot());
+        assertEquals(3, robot1.getLigne());
+        assertEquals(3, robot1.getColonne());
     }
 }
