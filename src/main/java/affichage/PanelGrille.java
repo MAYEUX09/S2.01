edd59855df2ee1d7393a4177acd405ad59f75f80
Ouameur <aissaouameur@gmail.com>
2026-05-20 14:01:34 +0200

    Mise à jour des icons et robot.jpg

--- src/main/java/affichage/PanelGrille.java
+++ src/main/java/affichage/PanelGrille.java
@@ -32,79 +32,74 @@ public class PanelGrille extends JPanel {
             for (int colonne = 0; colonne < 10; colonne++) {
                 Secteur secteur = monde.getGrille()[ligne][colonne];
 
-                JPanel caseSecteur = new JPanel(new GridLayout(2, 1));
+                JPanel caseSecteur = new JPanel(new BorderLayout());
                 caseSecteur.setPreferredSize(new Dimension(60, 60));
                 caseSecteur.setBorder(BorderFactory.createLineBorder(new Color(90, 90, 90), 1));
                 caseSecteur.setBackground(new Color(245, 245, 245));
 
-                JLabel haut = new JLabel("", SwingConstants.CENTER);
-                JLabel bas = new JLabel("", SwingConstants.CENTER);
-
-                styliserLabel(haut);
-                styliserLabel(bas);
-
                 if (secteur.getEau() != null) {
-                    caseSecteur.setLayout(new BorderLayout());
 
-                    ImageIcon icone = new ImageIcon(getClass().getClassLoader().getResource("images/ocean.jpg"));
-                    JLabel labelEau = new JLabel(new ImageIcon(icone.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
-                    labelEau.setHorizontalAlignment(SwingConstants.CENTER);
-                    labelEau.setVerticalAlignment(SwingConstants.CENTER);
-
-                    caseSecteur.add(labelEau, BorderLayout.CENTER);
+                    caseSecteur.add(creerLabelImage("images/ocean.jpg"), BorderLayout.CENTER);
 
                 } else if (secteur.getMine() == null && secteur.getEntrepot() == null && secteur.getRobot() == null) {
 
-                    caseSecteur.setLayout(new BorderLayout());
-
-                    ImageIcon icone = new ImageIcon(getClass().getClassLoader().getResource("images/terrain.jpg"));
-                    JLabel terrain = new JLabel(new ImageIcon(icone.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
-                    terrain.setHorizontalAlignment(SwingConstants.CENTER);
-                    terrain.setVerticalAlignment(SwingConstants.CENTER);
-
-                    caseSecteur.add(terrain, BorderLayout.CENTER);
+                    caseSecteur.add(creerLabelImage("images/terrain.jpg"), BorderLayout.CENTER);
 
                 } else {
+
+                    JLayeredPane calque = new JLayeredPane();
+                    calque.setPreferredSize(new Dimension(60, 60));
+
                     if (secteur.getMine() != null) {
-                        haut.setText("M" + secteur.getMine().getId());
-                        haut.setBackground(new Color(255, 204, 102));
-                        haut.setForeground(new Color(80, 50, 0));
+                        calque.add(creerLabelImageBounds("images/mine.jpg", 80, 80), JLayeredPane.DEFAULT_LAYER);
+                        calque.add(creerLabelId("M" + secteur.getMine().getId(), Color.WHITE), JLayeredPane.PALETTE_LAYER);
+
                     } else if (secteur.getEntrepot() != null) {
-                        haut.setText("E" + secteur.getEntrepot().getId());
-                        haut.setBackground(new Color(255, 235, 140));
-                        haut.setForeground(new Color(90, 70, 0));
+                        calque.add(creerLabelImageBounds("images/entrepot.jpg", 80, 80), JLayeredPane.DEFAULT_LAYER);
+                        calque.add(creerLabelId("E" + secteur.getEntrepot().getId(), Color.BLACK), JLayeredPane.PALETTE_LAYER);
+
                     } else {
-                        haut.setBackground(new Color(250, 250, 250));
+                        calque.add(creerLabelImageBounds("images/terrain.jpg", 60, 60), JLayeredPane.DEFAULT_LAYER);
                     }
 
                     if (secteur.getRobot() != null) {
-                        caseSecteur.setLayout(new BorderLayout());
-
-                        ImageIcon icone = new ImageIcon(getClass().getClassLoader().getResource("images/robot.jpg"));
-
-                        JLabel labelrobot = new JLabel(new ImageIcon(icone.getImage().getScaledInstance(85, 85, Image.SCALE_SMOOTH)));
-                        labelrobot.setHorizontalAlignment(SwingConstants.CENTER);
-                        labelrobot.setVerticalAlignment(SwingConstants.CENTER);
-
-                        caseSecteur.add(labelrobot, BorderLayout.CENTER);
-
-                        if (secteur.getMine() != null || secteur.getEntrepot() != null) {
-                            caseSecteur.add(haut, BorderLayout.NORTH);
-                        }
-
-                    } else {
-                        bas.setBackground(new Color(250, 250, 250));
-                        caseSecteur.add(haut);
-                        caseSecteur.add(bas);
+                        calque.add(creerLabelImageBounds("images/robot.jpg", 80, 70), JLayeredPane.MODAL_LAYER);
+                        calque.add(creerLabelId("R" + secteur.getRobot().getId(), Color.BLACK), JLayeredPane.POPUP_LAYER);
                     }
+
+                    caseSecteur.add(calque, BorderLayout.CENTER);
                 }
+
                 add(caseSecteur);
             }
         }
 
         revalidate();
         repaint();
+    }
 
+
+    private JLabel creerLabelImage(String chemin) {
+        ImageIcon icone = new ImageIcon(getClass().getClassLoader().getResource(chemin));
+        JLabel label = new JLabel(new ImageIcon(icone.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        return label;
+    }
+
+    private JLabel creerLabelImageBounds(String chemin, int largeur, int hauteur) {
+        ImageIcon icone = new ImageIcon(getClass().getClassLoader().getResource(chemin));
+        JLabel label = new JLabel(new ImageIcon(icone.getImage().getScaledInstance(largeur, hauteur, Image.SCALE_SMOOTH)));
+        label.setBounds(0, 0, largeur, hauteur);
+        return label;
+    }
+
+    private JLabel creerLabelId(String texte, Color couleur) {
+        JLabel label = new JLabel(texte);
+        label.setBounds(2, 2, 40, 20);
+        label.setFont(new Font("Arial", Font.BOLD, 12));
+        label.setForeground(couleur);
+        return label;
     }
 
 
